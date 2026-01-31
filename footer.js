b042376cec724a7c7a5a6cf8478a9d06980551f5
Dennis Kostka <kostka@Denniss-MacBook-Pro-2.local>
2026-01-31 11:47:09 -0500

    First version

---
+++ footer.js
@@ -0,0 +1,11 @@
+document.write(`
+    <footer>
+        <div class="footer-content">
+            <p>&copy; 2026 Kostka Lab. All rights reserved.</p>
+            <p>
+                <a href="https://scholar.google.com/citations?user=hkA4e-EAAAAJ&hl=en" style="color: #0066cc; text-decoration: none;">Google Scholar</a> | 
+                <a href="contact.html" style="color: #0066cc; text-decoration: none;">Contact</a>
+            </p>
+        </div>
+    </footer>
+`);
---
+++ header.js
@@ -0,0 +1,14 @@
+document.write(`
+    <header>
+        <nav>
+            <a href="index.html" class="logo">Kostka Lab</a>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="team.html">Team</a></li>
+                <li><a href="publications.html">Publications</a></li>
+                <li><a href="software.html">Software</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+`);
